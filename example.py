36737437956b5977d1b46d358ac031067c124c83
Igor Lapshin <56918497+ILapshin@users.noreply.github.com>
2020-05-04 21:34:41 +0300

    Add files via upload

---
+++ example.py
@@ -0,0 +1,8 @@
+# Example of using my Recognizer with wich I reached a 0.99528 accuracy
+
+from digit_recognizer import Recognizer
+
+a = Recognizer()
+result = a.fit(a.upgrade(10), save_weights=True, save_path='model/exapmle_weights.h5') # A directory .../model must already exists
+a.verify()
+a.output(a.predict(a.X_test), 'example_prediction')
